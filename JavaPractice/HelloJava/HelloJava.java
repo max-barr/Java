0d91b595dd57016774a485db68173567e000e641
Max Barr <mxbarr@gmail.com>
2022-08-25 14:14:35 -0700

    Practicing Java fundamentals

---
+++ JavaPractice/HelloJava/HelloJava.java
@@ -0,0 +1,14 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        int x = 22;
+        double dub = 1.25;
+        float flo = 13.5f;
+        String h = "How are you today?";
+        System.out.println(dub);
+        System.out.println(x);
+        System.out.println(h);
+        System.out.println(flo);
+    }
+}
