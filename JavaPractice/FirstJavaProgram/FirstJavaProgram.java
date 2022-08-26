eefd742e069f27bbd599e81864f165928a57017b
Max Barr <mxbarr@gmail.com>
2022-08-26 14:23:45 -0700

    Practicing Java fundamentals

---
+++ JavaPractice/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        String name = "Max";
+        int age = 30;
+        String hometown = "Portland";
+
+        System.out.printf("My name is %s.%n", name);
+        System.out.printf("I am %d years old.%n", age);
+        System.out.printf("My hometown is %s.%n", hometown);
+    }
+}
