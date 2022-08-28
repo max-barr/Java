8a94573ec5cffa123dec3cc6db56fe3dba69b442
Max Barr <mxbarr@gmail.com>
2022-08-28 16:33:52 -0700

    Practice importing classes

--- JavaPractice/ImportDemo/ImportDemo.java
+++ JavaPractice/ImportDemo/ImportDemo.java
@@ -3,7 +3,6 @@ import java.util.Date;
 public class ImportDemo {
     public String getCurrentDate() {
         Date date = new Date();
-        System.out.println("Today's date is " + date);
         return "Today's date is " + date;
     }
 }
--- JavaPractice/ImportDemo/ImportDemoTest.java
+++ JavaPractice/ImportDemo/ImportDemoTest.java
@@ -2,5 +2,6 @@ public class ImportDemoTest {
     public static void main(String[] args) {
         ImportDemo iD = new ImportDemo();
         String currentDate = iD.getCurrentDate();
+        System.out.println(currentDate);
     }
 }
