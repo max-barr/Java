432457552cbca479757cdaf0c5d98e4ca536d395
Max Barr <mxbarr@gmail.com>
2022-09-05 15:30:20 -0700

    Practice implementing inheritance with classes

--- JavaPractice/PracticeAssignments/ObjectMaster/Human/HumanTest.java
+++ JavaPractice/PracticeAssignments/ObjectMaster/Human/HumanTest.java
@@ -5,5 +5,25 @@ public class HumanTest {
 
         bill.attack(sandra);
         System.out.println(sandra.getHealth());
+
+        Ninja nate = new Ninja();
+        Wizard will = new Wizard();
+        Samurai steve = new Samurai();
+        Samurai sarah = new Samurai();
+
+        System.out.println(Samurai.howMany);
+
+        steve.deathBlow(sandra);
+        System.out.println(sandra.getHealth());
+
+        will.heal(sandra);
+        System.out.println(sandra.getHealth());
+
+        nate.steal(bill);
+        System.out.println(nate.getHealth());
+        System.out.println(bill.getHealth());
+
+        will.fireball(bill);
+        System.out.println(bill.getHealth());
     }
 }
