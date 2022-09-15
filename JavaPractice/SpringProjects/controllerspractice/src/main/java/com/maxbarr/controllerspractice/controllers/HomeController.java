1f9d7d4c8cf4f590ce080c0cf57df0341bd060f0
Max Barr <mxbarr@gmail.com>
2022-09-15 16:06:50 -0700

    Practice using query params

--- JavaPractice/SpringProjects/controllerspractice/src/main/java/com/maxbarr/controllerspractice/controllers/HomeController.java
+++ JavaPractice/SpringProjects/controllerspractice/src/main/java/com/maxbarr/controllerspractice/controllers/HomeController.java
@@ -1,19 +1,27 @@
 package com.maxbarr.controllerspractice.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String hello() {
-		return "Hello Controller! How are you?";
+	public String index(@RequestParam(value="q", required=false) String searchQuery) {
+		return "You searched for: " + searchQuery;
 	}
 	
 	@RequestMapping("/goodbye")
 	public String goodbye() {
 		return "Goodbye. It's been fun.";
 	}
-
+	
+	@RequestMapping("/users/{name}")
+	public String showName(@PathVariable("name") String userName) {
+		System.out.println(userName);
+		return "The user name is: " + userName;
+	}
+	
 }
