8bd4938dd4fa868ae1be0b9aeead4c5862ae2414
Max Barr <mxbarr@gmail.com>
2022-09-28 16:37:42 -0700

    Practice using session in STS

---
+++ JavaPractice/SpringProjects/sessioncounter/src/main/java/com/maxbarr/sessioncounter/SessioncounterApplication.java
@@ -0,0 +1,13 @@
+package com.maxbarr.sessioncounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessioncounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessioncounterApplication.class, args);
+	}
+
+}
---
+++ JavaPractice/SpringProjects/sessioncounter/src/test/java/com/maxbarr/sessioncounter/SessioncounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxbarr.sessioncounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessioncounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
