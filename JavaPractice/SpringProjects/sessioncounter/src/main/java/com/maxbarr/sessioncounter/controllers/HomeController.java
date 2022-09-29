1b2c42a2736e63591bb1d046a77772f74b0e28dc
Max Barr <mxbarr@gmail.com>
2022-09-28 17:08:11 -0700

    Practice using session in STS project

--- JavaPractice/SpringProjects/sessioncounter/src/main/java/com/maxbarr/sessioncounter/controllers/HomeController.java
+++ JavaPractice/SpringProjects/sessioncounter/src/main/java/com/maxbarr/sessioncounter/controllers/HomeController.java
@@ -3,19 +3,28 @@ package com.maxbarr.sessioncounter.controllers;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 	
-	@GetMapping("/counter")
+	@GetMapping("/")
 	public String index(HttpSession session) {
 		if (session.getAttribute("count") == null) {
 			session.setAttribute("count", 0);
 		} else {
-			session.setAttribute("count", (Integer) session.getAttribute("count") + 1);
+			Integer currentCount = (Integer) session.getAttribute("count");
+			session.setAttribute("count", currentCount + 1);
 		}
-		return "index.jsp";
+		return "home.jsp";
+	}
+	
+	@GetMapping("/count")
+	public String showCount(HttpSession session, Model model) {
+		Integer currentCount = (Integer) session.getAttribute("count");
+		model.addAttribute("countToShow", currentCount);
+		return "count.jsp";
 	}
 
 }
