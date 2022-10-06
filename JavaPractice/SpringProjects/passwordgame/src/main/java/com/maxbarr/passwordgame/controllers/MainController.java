455967b9b6ae5398609dc8cc2569f24e667f174c
Max Barr <mxbarr@gmail.com>
2022-10-06 15:17:25 -0700

    Update controller in practice project

--- JavaPractice/SpringProjects/passwordgame/src/main/java/com/maxbarr/passwordgame/controllers/MainController.java
+++ JavaPractice/SpringProjects/passwordgame/src/main/java/com/maxbarr/passwordgame/controllers/MainController.java
@@ -1,5 +1,27 @@
 package com.maxbarr.passwordgame.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class MainController {
+	
+	@GetMapping("/")
+	public String home() {
+		
+	}
+	
+	
+	@GetMapping("/password")
+	
+	
+	@PostMapping("/")
+	public String enterPassword() {
+		
+		return "redirect:/password";
+	}
+	
+	
 
 }
