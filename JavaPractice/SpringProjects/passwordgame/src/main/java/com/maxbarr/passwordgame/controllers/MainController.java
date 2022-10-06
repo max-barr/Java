33a117f19bc0c6e7e1d563aa465927313145a851
Max Barr <mxbarr@gmail.com>
2022-10-06 15:48:33 -0700

    Practice project implementing error msgs

--- JavaPractice/SpringProjects/passwordgame/src/main/java/com/maxbarr/passwordgame/controllers/MainController.java
+++ JavaPractice/SpringProjects/passwordgame/src/main/java/com/maxbarr/passwordgame/controllers/MainController.java
@@ -2,29 +2,42 @@ package com.maxbarr.passwordgame.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
 	
+//	home page route
 	@GetMapping("/")
-	public String home() {
-		
+	public String home(@ModelAttribute("error") String error) {
 		return "home.jsp";
 	}
 	
+//	Errors
+	@GetMapping("/errors")
+	public String error(RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("error", "That is incorrect!")
+		return "redirect:/";
+	}
 	
+//	Password success route
 	@GetMapping("/password")
 	public String accept() {
-		
 		return "dashboard.jsp";
 	}
 	
-	
+//	Post route to grab data from form
 	@PostMapping("/login")
-	public String enterPassword() {
-		
-		return "redirect:/password";
+	public String enterPassword(@RequestParam(value="password") String password) {
+		if (password.equals("code")) {
+			System.out.println("That is correct");
+			return "redirect:/password";
+		} else {
+			return "redirect:/error";
+		}
 	}
 	
 	
