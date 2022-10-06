dfa1a32c08dceffc8d07fe280a6f2d508ad2e69a
Max Barr <mxbarr@gmail.com>
2022-10-06 14:51:54 -0700

    Practice project using error messages

---
+++ JavaPractice/SpringProjects/passwordgame/src/main/java/com/maxbarr/passwordgame/PasswordgameApplication.java
@@ -0,0 +1,13 @@
+package com.maxbarr.passwordgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordgameApplication.class, args);
+	}
+
+}
