eeeb931d0c99999ec90d9917dd90e8719b64cabf
Max Barr <mxbarr@gmail.com>
2022-10-27 16:23:30 -0700

    Added request params to controller

--- JavaPractice/SpringProjects/surveyform/src/main/java/com/maxbarr/surveyform/controllers/MainController.java
+++ JavaPractice/SpringProjects/surveyform/src/main/java/com/maxbarr/surveyform/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.maxbarr.surveyform.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -21,7 +22,7 @@ public class MainController {
 	
 //	POST route to grab data from form
 	@PostMapping(path="/submit")
-	public String submitForm() {
+	public String submitForm(@RequestParam(value="name") String name, @RequestParam(value="location") String location, @RequestParam(value="sport") String sport, @RequestParam(value="comment") String comment) {
 		return "redirect:/result";
 	}
 
