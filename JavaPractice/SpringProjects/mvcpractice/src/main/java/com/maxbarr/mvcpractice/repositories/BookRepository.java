613825e6b454d2eb85efb8ec72c6de61aaf70928
Max Barr <mxbarr@gmail.com>
2022-11-02 15:40:43 -0700

    Added repository

--- JavaPractice/SpringProjects/mvcpractice/src/main/java/com/maxbarr/mvcpractice/repositories/BookRepository.java
+++ JavaPractice/SpringProjects/mvcpractice/src/main/java/com/maxbarr/mvcpractice/repositories/BookRepository.java
@@ -1,5 +1,7 @@
 package com.maxbarr.mvcpractice.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,15 @@ import com.maxbarr.mvcpractice.models.Book;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 
+//	Retrieve all books
+	List<Book> findAll();
+	
+//	Finds books with descriptions containing the search string
+	List<Book> findByDescriptionContaining(String search);
+	
+    // Counts how many titles contain a certain string
+    Long countByTitleContaining(String search);
+    
+    // Deletes a book that starts with a specific title
+    Long deleteByTitleStartingWith(String search);
 }
