a72075f9c24d6dff64022f6229e754d63ff20942
Max Barr <mxbarr@gmail.com>
2022-11-03 15:48:56 -0700

    Added controller

---
+++ JavaPractice/SpringProjects/mvcpractice/src/main/java/com/maxbarr/mvcpractice/controllers/BooksApi.java
@@ -0,0 +1,40 @@
+package com.maxbarr.mvcpractice.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maxbarr.mvcpractice.models.Book;
+import com.maxbarr.mvcpractice.services.BookService;
+
+@RestController
+public class BooksApi {
+//	import service
+	private final BookService bookService;
+	
+	public BooksApi(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@GetMapping("/api/books")
+	public List<Book> index() {
+		return bookService.allBooks();
+	}
+	
+	@PostMapping("/api/books")
+	public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+		Book book = new Book(title, desc, lang, numOfPages);
+		return bookService.createBook(book);
+	}
+	
+	@GetMapping("/api/books/{id}")
+	public Book show(@PathVariable("id") Long id) {
+		Book book = bookService.findBook(id);
+		return book;
+	}
+
+}
