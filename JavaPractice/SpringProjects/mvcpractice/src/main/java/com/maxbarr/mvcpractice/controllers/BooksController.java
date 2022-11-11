6865cdb6f9e09d208596d50a14ede9a3c6e85ebf
Max Barr <mxbarr@gmail.com>
2022-11-10 16:47:09 -0800

    Updated controller

--- JavaPractice/SpringProjects/mvcpractice/src/main/java/com/maxbarr/mvcpractice/controllers/BooksController.java
+++ JavaPractice/SpringProjects/mvcpractice/src/main/java/com/maxbarr/mvcpractice/controllers/BooksController.java
@@ -1,8 +1,28 @@
 package com.maxbarr.mvcpractice.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.maxbarr.mvcpractice.models.Book;
+import com.maxbarr.mvcpractice.services.BookService;
 
 @Controller
 public class BooksController {
+	
+	private final BookService bookService;
+	
+	public BooksController(BookService bookService) {
+		this.bookService = bookService;
+	}
+	
+	@GetMapping("/books")
+	public String index(Model model) {
+		List<Book> books = bookService.allBooks();
+		model.addAttribute("books", books);
+		return "/books/index.jsp";
+	}
 
 }
