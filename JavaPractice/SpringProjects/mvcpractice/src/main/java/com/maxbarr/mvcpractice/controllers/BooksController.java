ff482b7e0c3de926963018c7cd167cfe83cc9ee5
Max Barr <mxbarr@gmail.com>
2022-11-17 15:32:28 -0800

    Updated controller. Added methods

--- JavaPractice/SpringProjects/mvcpractice/src/main/java/com/maxbarr/mvcpractice/controllers/BooksController.java
+++ JavaPractice/SpringProjects/mvcpractice/src/main/java/com/maxbarr/mvcpractice/controllers/BooksController.java
@@ -2,10 +2,14 @@ package com.maxbarr.mvcpractice.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.maxbarr.mvcpractice.models.Book;
 import com.maxbarr.mvcpractice.services.BookService;
@@ -32,6 +36,13 @@ public class BooksController {
 	}
 	
 	@PostMapping("/books")
-	
+	public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+		if (result.hasErrors()) {
+			return "new.jsp";
+		} else {
+			bookService.createBook(book);
+			return "redirect:/books";
+		}
+	}
 
 }
